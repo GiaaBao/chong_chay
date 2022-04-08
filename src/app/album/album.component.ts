8c6e8cb909926a0b16d47eaee60da7233e353f24
GiaBao <91052100+GiaaBao@users.noreply.github.com>
2022-04-08 22:03:13 +0700

    update new

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import music from '../../../db.json';
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+  limit = music.splice(0,7);
 
   constructor() { }
 
