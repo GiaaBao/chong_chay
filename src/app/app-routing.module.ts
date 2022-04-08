f1230849bf28b288f4458b1b017de8d56787739d
GiaBao <91052100+GiaaBao@users.noreply.github.com>
2022-04-08 20:38:39 +0700

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './album/album.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+// đường dẫn trên web lúc clik
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'album', component: AlbumComponent},
+  { path: 'pagenotfound', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
